a5e024ca97b34afdc51c81b4afa4fb06a350ab67
Redwane ZAFARI <rzafari@e2r8p8.42.fr>
2021-09-27 12:31:17 +0200

    Everything seems to work well for Stack container

---
+++ stack.hpp
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/27 10:01:55 by rzafari           #+#    #+#             */
+/*   Updated: 2021/09/27 12:30:38 by rzafari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STACK_HPP
+# define STACK_HPP
+#include "stack_class.hpp"
+
+namespace ft
+{
+    template < class T, class Container >
+    stack<T, Container>::stack(const container_type& ctnr) : _ctnr(ctnr) {}
+
+    template < class T, class Container >
+    stack<T, Container>::~stack()
+    {
+        return ;
+    }
+
+    template < class T, class Container >
+    bool stack<T, Container>::empty() const
+    {
+        return _ctnr.empty();
+    }
+
+    template < class T, class Container >
+    typename stack<T, Container>::size_type stack<T, Container>::size() const
+    {
+        return _ctnr.size();
+    }
+
+    template < class T, class Container >
+    typename stack<T, Container>::value_type& stack<T, Container>::top()
+    {
+        return _ctnr.back();
+    }
+
+    template < class T, class Container >
+    const typename stack<T, Container>::value_type&  stack<T, Container>::top() const
+    {
+        return _ctnr.back();
+    }
+
+    template < class T, class Container >
+    void stack<T, Container>::push(const value_type& val)
+    {
+        return _ctnr.push_back(val);
+    }
+
+    template <class T, class Container >
+    void stack<T, Container>::pop()
+    {
+        return _ctnr.pop_back();
+    }
+
+    template <class T, class Container>
+    bool operator==(const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+    {
+        return (lhs._ctnr == rhs._ctnr);
+    }
+
+    template <class T, class Container>
+    bool operator!=(const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    template <class T, class Container>
+    bool operator<(const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+    {
+        return lhs._ctnr < rhs._ctnr;
+    }
+
+    template <class T, class Container>
+    bool operator<=(const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+    {
+        return !(rhs < lhs);
+    }
+
+    template <class T, class Container>
+    bool operator>(const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+    {
+        return (rhs < lhs);
+    }
+
+    template <class T, class Container>
+    bool operator>=(const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+    {
+        return !(lhs < rhs);
+    }
+
+}
+
+#endif
---
+++ stack_class.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_class.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzafari <rzafari@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/27 09:39:49 by rzafari           #+#    #+#             */
+/*   Updated: 2021/09/27 12:26:29 by rzafari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STACK_CLASS_HPP
+# define STACK_CLASS_HPP
+# include "vector.hpp"
+# include <stack>
+
+namespace ft
+{
+    template <class T, class Container = ft::vector<T> >
+    class stack
+    {
+        public:
+            typedef T           value_type;
+            typedef Container   container_type;
+            typedef size_t      size_type;
+
+            explicit            stack (const container_type& ctnr = container_type());
+            virtual             ~stack();
+            bool                empty() const;
+            size_type           size() const;
+            value_type&         top();
+            const value_type&   top() const;
+            void                push (const value_type& val);
+            void                pop();
+        
+            container_type      _ctnr;
+    };
+    
+    //Non-member function overloads
+    //Relational operators
+    template <class T, class Container>
+        bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+    template <class T, class Container>
+        bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+    template <class T, class Container>
+        bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+    template <class T, class Container>
+        bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+    template <class T, class Container>
+        bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+    template <class T, class Container>
+        bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+}
+
+#endif
