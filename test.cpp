96f6919d163dba6eb74961ad5120f1d86d9fc1e0
rzafari42 <redwane.zafari@hotmail.com>
2021-11-10 18:01:55 +0100

    allocator::rerbind added according to the subject recommendation

--- test.cpp
+++ test.cpp
@@ -1,14 +1,23 @@
-#include "map_class.hpp"
+/*#include "map_class.hpp"
 #include "map.hpp"
 #include "utils.hpp"
+#include "MapIter.hpp"*/
+#include "marc_tester/srcs/map/common.hpp"
 
-/*struct ft_more {
-	bool	operator()(const int &first, const std::string &second) const {
+#define T1 int
+#define T2 std::string
+
+struct ft_more {
+	bool	operator()(const T1 &first, const T1 &second) const {
 		return (first > second);
 	}
-};*/
+};
 
-template <typename T>
+typedef ft::map<T1, T2, ft_more > ft_mp;
+typedef ft::map<T1, T2, ft_more >::iterator iterator;
+typedef ft::pair<T1, T2> pair;
+
+/*template <typename T>
 std::string	printPair(const T &iterator, bool nl = true, std::ostream &o = std::cout)
 {
 	o << "key: " << iterator->first << " | value: " << iterator->second;
@@ -30,13 +39,10 @@ void	printSize(T_MAP const &mp, bool print_content = 1)
 			std::cout << "- " << printPair(it, false) << std::endl;
 	}
 	std::cout << "###############################################" << std::endl;
-}
-
+}*/
 
 int main(void)
 {
-	ft::map<int, std::string> mp;
-    
     //ft::map<int, std::string>::iterator ite;
 
     /*mp[42] = "fgzgxfn";
@@ -55,14 +61,24 @@ int main(void)
 	printSize(mp);
 	mp[90] = "8";
 	printSize(mp);*/
-	
-	mp[42] = "fgzgxfn";
+
+	ft_mp mp;
+	iterator ret;
+	iterator it;
+
+	ret = mp.insert(it, pair(42, "fgzgxfn"));
+	ret = mp.insert(it, pair(25, "funny"));
+	ret = mp.insert(it, pair(80, "hey"));
+	ret = mp.insert(it, pair(12, "no"));
+	ret = mp.insert(it, pair(27, "bee"));
+	ret = mp.insert(it, pair(90, "8"));
+	printSize(mp);
+
+	/*mp[42] = "fgzgxfn";
 	mp[25] = "funny";
 	mp[80] = "hey";
 	mp[12] = "no";
 	mp[27] = "bee";
-	mp[90] = "8";
-	printSize(mp);
-
+	mp[90] = "8";*/
     return 0;
 }
